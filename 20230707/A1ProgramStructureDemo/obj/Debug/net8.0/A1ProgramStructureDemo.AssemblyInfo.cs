af71832a5bbc73916dddaa986eadbcd1ca85ef1c
ravikiranchalla09 <ravikiran.challa09@gmail.com>
2023-07-20 09:12:33 +0530

    commit
    commit

--- 20230707/A1ProgramStructureDemo/obj/Debug/net8.0/A1ProgramStructureDemo.AssemblyInfo.cs
+++ 20230707/A1ProgramStructureDemo/obj/Debug/net8.0/A1ProgramStructureDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("A1ProgramStructureDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f863fb6b0214031e2167e3c999fd9d22b1aa4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("A1ProgramStructureDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("A1ProgramStructureDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
